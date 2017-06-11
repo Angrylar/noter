707a1c331aefd6b63f85a1714e5497b7eb0cb56a
AllStack <renxiangyu@dafenci.com>
2017-06-11 23:32:38 +0800

    change some style

--- src/components/index/index.vue
+++ src/components/index/index.vue
@@ -1,27 +1,28 @@
 <style lang="less">
-    @import url('./index.less');
+  @import url('./index.less');
+
 </style>
 <template>
-    <div id="index">
-        <div class="index-header">
-            <p class="app-name" @click='goSetting()'>笔记</p>
-            <p class="searcher-icon"></p>
-        </div>
-        <div class="index-main">
-            <div class="index-main-inner">
-                <ul>
-                    <li v-for='obj of noteList' @click='showPreView(obj.nid)'>
-                        <span class="note-tittle">{{obj.tittle}}</span>
-                        <span class="modify-time">{{obj.time}}</span>
-                    </li>
-                </ul>
-            </div>
-            <div class="loading-bar" v-show='loadingShow'>
-                <p>----{{loadingMsg}}----</p>
-            </div>
-        </div>
-        <div class="add-new-note" @click='addNewNote()'></div>
+  <div id="index">
+    <div class="index-header">
+      <p class="app-name" @click='goSetting()'>笔记</p>
+      <p class="searcher-icon"></p>
     </div>
+    <div class="index-main">
+      <div class="index-main-inner">
+        <ul> 
+          <li v-for='obj of noteList' @click='showPreView(obj.nid)'>
+            <span class="note-tittle">{{obj.tittle}}</span>
+            <span class="modify-time">{{obj.time}}</span>
+          </li>
+        </ul>
+        <div class="loading-bar" v-show='loadingShow'>
+          <p>----{{loadingMsg}}----</p>
+        </div>
+      </div>
+    </div>
+    <div class="add-new-note" @click='addNewNote()'></div>
+  </div>
 </template>
 
 <script src="./index.js"></script>
